b2a67d74417c45d0068122b46dda7795f67ce9a2
Mohit Sharma <mohit.786.sharma@gmail.com>
2015-12-29 00:48:55 +0530

    added svd computation using lapack

---
+++ svdLapack.cpp
@@ -0,0 +1,84 @@
+#include "svdLapack.h"
+
+void svdUsingLapack(gk_csr_t *mat, int rank, 
+    std::vector<std::vector<double>>& uFac, 
+    std::vector<std::vector<double>>& iFac) {
+
+  int nrows = mat->nrows;
+  int ncols = mat->ncols;
+  int u, k, ii, item, min_mn;
+  double rating;
+
+  //declare variables for svd comp
+  char jobz = 'S';
+  double *a, *U, *Vt, *S, *work;
+  double wkopt;
+  int m, n, lda, ldu, ldvt, info, lwork;
+  int *iWork;
+
+  //create dense matrix
+  double *dMat  = (double*) malloc(sizeof(double)*nrows*ncols);
+  memset(dMat, 0, sizeof(double)*nrows*ncols);
+  
+  for (u = 0; u < nrows; u++) {
+    for (ii = mat->rowptr[u]; ii < mat->rowptr[u+1]; ii++) {
+      item = mat->rowind[ii];
+      rating = mat->rowval[ii];
+      dMat[u*ncols + item] = rating;    
+    }
+  }
+
+  //define variables for svd comp  min_mn = ncols;
+  min_mn = ncols;
+  if (nrows < ncols) {
+    min_mn = nrows;
+  }
+  m = nrows, n = ncols;
+  lda = m, ldu =  m, ldvt = n;
+  a = dMat;
+  iWork = (int*) malloc(sizeof(int)*8*min_mn);  
+  S = (double*) malloc(sizeof(double)*min_mn); 
+  U = (double*) malloc(sizeof(double)*ldu*min_mn);
+  Vt = (double*) malloc(sizeof(double)*ldvt*min_mn);
+
+  /* Query and allocate the optimal workspace */
+  lwork= -1; 
+  dgesdd_(&jobz, &m, &n, a, &lda, S, U, &ldu, Vt, &ldvt, &wkopt, &lwork, iWork,
+      &info);
+  lwork = (int)wkopt;
+  work = (double*) malloc(sizeof(double)*lwork);
+  
+  //compute SVD
+  dgesdd_(&jobz, &m, &n, a, &lda, S, U, &ldu, Vt, &ldvt, &wkopt, &lwork, iWork,
+      &info);
+
+  /* Check for convergence */
+  if( info > 0 ) {
+    std::cout << "The algorithm computing SVD failed to converge.\n";
+    exit(1);
+  }
+
+  //copy left-singular vectors to uFac
+  for (u = 0; u < nrows; u++) {
+    for (k = 0; k < rank; k++) {
+      uFac[u][k] = U[u*min_mn + k]; 
+    }
+  }
+
+  //copy right-singular vectors to iFac
+  for (item = 0; item < ncols; item++) {
+    for (k = 0; k < rank; k++) {
+      iFac[item][k] = Vt[k*ncols + item];
+    }
+  }
+  
+
+  free(work);
+  free(U);
+  free(Vt);
+  free(S);
+  free(dMat);
+  free(iWork);
+}
+
+
---
+++ svdLapack.h
@@ -0,0 +1,20 @@
+#ifndef _SVD_LAPACK_H_
+#define _SVD_LAPACK_H_
+
+#include <cstdlib>
+#include <iostream>
+#include <vector>
+#include "GKlib.h"
+
+/* DGESDD prototype */
+extern "C" {
+  void dgesdd_( char* jobz, int* m, int* n, double* a,
+                int* lda, double* s, double* u, int* ldu, double* vt, int* ldvt,
+                double* work, int* lwork, int* iwork, int* info );
+}
+
+void svdUsingLapack(gk_csr_t *mat, int rank, 
+    std::vector<std::vector<double>>& uFac, 
+    std::vector<std::vector<double>>& iFac);
+
+#endif
