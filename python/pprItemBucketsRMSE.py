712b46e21ec0348c3724d6a666a30241d83bc8c8
Mohit Sharma <mohit.786.sharma@gmail.com>
2016-01-28 21:23:54 -0600

    finished random walk draft code

--- python/pprItemBucketsRMSE.py
+++ python/pprItemBucketsRMSE.py
@@ -6,8 +6,12 @@ import numpy as np
 def computeBucketsRMSE(pprFName, uFac, iFac, origUFac, origIFac, 
     nBuckets):
   nItems = iFac.shape[1]
+  nUsers = uFac.shape[1]
   u = 0
   itemsPerBuck = nItems/nBuckets
+
+  print 'itemsPerBuck: ', itemsPerBuck
+
   bucketsScore = [0 for i in range(nBuckets)]
 
   with open(pprFName, 'r') as f:
@@ -19,16 +23,18 @@ def computeBucketsRMSE(pprFName, uFac, iFac, origUFac, origIFac,
         for j in range(i, i+(itemsPerBuck*2), 2):
           if j >= len(cols)-1:
             break
-          item = cols[j]
+          item = int(cols[j]) - nUsers
           origRat = np.dot(origUFac[u], origIFac[item])
           estRat = np.dot(uFac[u], iFac[item])
           se += (origRat - estRat)*(origRat - estRat)
           nnz += 1
         rmse = np.sqrt(se/nnz)
-        bucketsScore[] += rmse
+        bucketsScore[i%(itemsPerBuck*2)] += rmse
       u += 1
-      
-
+  for i in range(nBuckets):
+    bucketsScore[i] = bucketsScore[i]/u
+  print 'buckets score: ', bucketsScore
+  return bucketsScore
 
 
 def main():
@@ -50,6 +56,8 @@ def main():
   print 'iFac shape: ', iFac.shape
   nItems = iFac.shape[0]
   print 'nItems: ', nItems
+  
+  computeBucketsRMSE(pprFName, uFac, iFac, origUFac, origIFac, nBuckets)
 
 
 if __name__ == '__main__':
