f6ca929d2ea1f15b2d38d08114bddaf6b31a4e78
mohit sharma <sharm163@umn.edu>
2016-02-03 16:05:36 -0600

    added global pagerank computation

--- computePPR.cpp
+++ computePPR.cpp
@@ -39,10 +39,11 @@ std::vector<std::pair<int,float>> getSortedItems(int u, gk_csr_t *mat,
 
 
 void writeSortedItemsByPR(gk_csr_t *mat, int nUsers, int nItems, float lambda,
-    int max_niter) {
-  std::ofstream opFile("prItems.txt");
+    int max_niter, char *opFileName) {
+  std::ofstream opFile(opFileName);
   if (opFile.is_open()) {
     for (int u = 0; u < nUsers; u++) {
+      float sum = 0;
       std::vector<std::pair<int, float>> itemScores = getSortedItems(u, mat,
           lambda, max_niter, nUsers, nItems);
       if (itemScores.size() != nItems) {
@@ -50,10 +51,11 @@ void writeSortedItemsByPR(gk_csr_t *mat, int nUsers, int nItems, float lambda,
       }
       for (int i = 0; i < nItems; i++) {
         opFile << itemScores[i].first << " " << itemScores[i].second << " ";
+        sum += itemScores[i].second;
       }
       opFile << std::endl;
       if (u % 500 == 0) {
-        std::cout << "\nDone users " << u << "..." << std::endl;
+        std::cout << "\nDone users " << u << "..." << " sum: " << sum << std::endl;
       }
     }
     opFile.close();
@@ -61,18 +63,62 @@ void writeSortedItemsByPR(gk_csr_t *mat, int nUsers, int nItems, float lambda,
 }
 
 
+void writeSortedItemsByGlobalPR(gk_csr_t *mat, int nUsers, int nItems, float lambda,
+    int max_niter, char *opFileName) {
+  //custom compare function to sort vector of pairs in decreasing order
+  auto comparePair = [](std::pair<int, float> a, std::pair<int, float> b) { 
+    return a.second > b.second; 
+  };
+  
+  std::ofstream opFile(opFileName);
+  float *pr = (float*) malloc(sizeof(float)*mat->nrows);
+  memset(pr, 0, sizeof(float)*mat->nrows);
+  float sm = 0;
+  for (int u = 0; u < nUsers; u++) {
+    pr[u] = 1.0/nUsers;
+    sm += pr[u];
+  }
+  
+  std::cout << "\nsum pr: " << sm << std::endl;
+ 
+  gk_rw_PageRank(mat, lambda, 0.0001, max_niter, pr);
+  
+  //get pr score of items
+  std::vector<std::pair<int, float>> itemScores;
+  for (int i = nUsers; i < nUsers + nItems; i++) {
+    itemScores.push_back(std::make_pair(i, pr[i]));
+  }
+  
+  //sort items by their pr scores in decreasing order
+  std::sort(itemScores.begin(), itemScores.end(), comparePair);
+
+  if (opFile.is_open()) {
+    if (itemScores.size() != nItems) {
+      std::cerr << "\nscore not computed for suff items." << std::endl;
+    }
+    for (int i = 0; i < nItems; i++) {
+      opFile << itemScores[i].first << " " << itemScores[i].second << std::endl;
+    }
+    opFile.close();
+  }
+
+  free(pr);
+}
+
+
 int main(int argc, char* argv[]) {
 
-  if (argc < 6) {
+  if (argc < 7) {
     std::cerr << "\nInsufficient arguments" << std::endl;
     exit(0);
   }
 
-  char* ipCSRAdj = argv[1];
-  int nUsers     = atoi(argv[2]);
-  int nItems     = atoi(argv[3]);
-  float lambda   = atof(argv[4]);
-  int max_niter  = atoi(argv[5]);
+  char* ipCSRAdj   = argv[1];
+  char* opFileName = argv[2];
+  int nUsers       = atoi(argv[3]);
+  int nItems       = atoi(argv[4]);
+  float lambda     = atof(argv[5]);
+  int max_niter    = atoi(argv[6]);
 
   gk_csr_t *graphMat = gk_csr_Read(ipCSRAdj, GK_CSR_FMT_CSR, 0, 0);
 
@@ -81,12 +127,12 @@ int main(int argc, char* argv[]) {
   std::cout << "\nnUsers: " << nUsers;
   std::cout << "\nnItems: " << nItems;
 
-  writeSortedItemsByPR(graphMat, nUsers, nItems, lambda, max_niter);
+  //writeSortedItemsByPR(graphMat, nUsers, nItems, lambda, max_niter, opFileName);
+  writeSortedItemsByGlobalPR(graphMat, nUsers, nItems, lambda, max_niter, 
+      opFileName);
 
   gk_csr_Free(&graphMat);
   return 0;
 }
-
-
 
 
--- python/pprItemBucketsRMSE.py
+++ python/pprItemBucketsRMSE.py
@@ -2,7 +2,6 @@ import sys
 import numpy as np
 
 
-
 def computeBucketsRMSE(pprFName, uFac, iFac, origUFac, origIFac, 
     nBuckets):
   nItems = iFac.shape[0]
@@ -14,6 +13,7 @@ def computeBucketsRMSE(pprFName, uFac, iFac, origUFac, origIFac,
 
   bucketsScore = [0 for i in range(nBuckets+1)]
   bucketsNNZ = [0 for i in range(nBuckets+1)]
+  usersBucketProp = {}
 
   with open(pprFName, 'r') as f:
     for line in f:
@@ -23,6 +23,9 @@ def computeBucketsRMSE(pprFName, uFac, iFac, origUFac, origIFac,
         
         nnz = 0
         se = 0.0
+        
+        first50PcItems = 0.0
+        second50PcItems = 0.0
 
         for j in range(i, i+(itemsPerBuck*2), 2):
           
@@ -31,6 +34,12 @@ def computeBucketsRMSE(pprFName, uFac, iFac, origUFac, origIFac,
             break
           
           item    = int(cols[j]) - nUsers
+          
+          if item < 10000:
+            first50PcItems += 1
+          else:
+            second50PcItems += 1
+
           origRat = np.dot(origUFac[u], origIFac[item])
           estRat  = np.dot(uFac[u], iFac[item])
 
@@ -41,19 +50,84 @@ def computeBucketsRMSE(pprFName, uFac, iFac, origUFac, origIFac,
         #print 'i:', i, 'bucketId: ', bucketId
         bucketsScore[bucketId] += se
         bucketsNNZ[bucketId] += nnz
+
+        if u not in usersBucketProp:
+          usersBucketProp[u] = []
+        usersBucketProp[u].append((bucketId,first50PcItems/nnz,
+          second50PcItems/nnz))
         bucketId += 1
 
       if 0 == u%1000:
         print u, ' done...'
         print 'buckets score: ', bucketsScore
         print 'bucketsNNZ: ', bucketsNNZ
+        print 'bucketsProp: ', usersBucketProp[u]
 
       u += 1
 
-  for i in range(nBuckets):
-    bucketsScore[i] = np.sqrt(bucketsScore[i]/bucketsNNZ[i])
+  for i in range(nBuckets+1):
+    if bucketsNNZ[i] > 0:
+      bucketsScore[i] = np.sqrt(bucketsScore[i]/bucketsNNZ[i])
   print 'Final buckets score: ', bucketsScore
+
+  with open(pprFName+'uBucketsProp.txt', 'w') as g:
+    for u in range(nUsers):
+      for x in usersBucketProp[u]:
+        g.write(str(x[0]) + ':' + str(x[1]) + ':' + str(x[2]) + ' ')
+      g.write('\n')
+
   return bucketsScore
+
+
+def computeBucketsRMSE4mGlobal(pprFName, uFac, iFac, origUFac, origIFac, 
+    nBuckets):
+  
+  nItems = iFac.shape[0]
+  nUsers = uFac.shape[0]
+  itemsPerBuck = nItems/nBuckets
+  print 'nItems: ', nItems, 
+  print 'itemsPerBuck: ', itemsPerBuck
+
+  bucketsScore = []
+  prItemsRMSEs = []
+  prItemsSEs = []
+
+  with open(pprFName, 'r') as f:
+    for line in f:
+      cols = line.strip().split()
+      item = int(cols[0]) - nUsers
+      #accumulate square error for the item
+      se = 0
+      for u in range(nUsers):
+        origRat = np.dot(origUFac[u], origIFac[item])
+        estRat = np.dot(uFac[u], iFac[item])
+        se += (origRat - estRat)*(origRat - estRat)
+      prItemsSEs.append(se)
+      prItemsRMSEs.append(np.sqrt(se/nUsers))
+
+  for i in range(nBuckets+1):
+    startItem = i*itemsPerBuck
+    endItem = (i+1)*itemsPerBuck
+    if endItem > nItems:
+      endItem = nItems
+    bucketSE = 0
+    for j in range(startItem, endItem):
+      bucketSE += prItemsSEs[j]
+    if endItem-startItem > 0:
+      bucketRMSE = np.sqrt(bucketSE/(nUsers*(endItem-startItem)))
+      bucketsScore.append(bucketRMSE)
+
+  print 'bucketsScore: ', bucketsScore
+
+  with open(pprFName + 'gprItemsRMSE.txt', 'w') as g:
+    for iRMSE in prItemsRMSEs:
+      g.write(str(iRMSE) + '\n')
+
+  with open(pprFName + 'gBuckRMSE.txt', 'w') as g:
+    for buckScore in bucketsScore:
+      g.write(str(buckScore) + '\n')
+  
+  return (prItemsRMSEs, bucketsScore)
 
 
 def main():
@@ -77,8 +151,8 @@ def main():
   print 'nItems: ', nItems
   print 'nBuckets: ', nBuckets
 
-  computeBucketsRMSE(pprFName, uFac, iFac, origUFac, origIFac, nBuckets)
-
+  #computeBucketsRMSE(pprFName, uFac, iFac, origUFac, origIFac, nBuckets)
+  computeBucketsRMSE4mGlobal(pprFName, uFac, iFac, origUFac, origIFac, nBuckets)
 
 if __name__ == '__main__':
   main()
