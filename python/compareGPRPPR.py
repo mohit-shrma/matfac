24ebdfc18ca82300c4213d96ec9d471c50f8bed3
mohit sharma <sharm163@umn.edu>
2016-02-28 21:32:00 -0600

    added item frequency display

--- python/compareGPRPPR.py
+++ python/compareGPRPPR.py
@@ -6,7 +6,8 @@ def findBetterPPR(pprFName, gprFName):
   pprF = open(pprFName, 'r')
   gprF = open(gprFName, 'r')
   u = 0
-  pprBetter = 0 
+  pprBetter = 0
+  betterUs = []
   for pprLine in pprF:
     gprLine = gprF.readline()
     
@@ -18,21 +19,36 @@ def findBetterPPR(pprFName, gprFName):
       #print 'user: ', u
       #print 'ppr: ', pprs
       #print 'gpr: ', gprs
-      pprBetter += 1  
+      pprBetter += 1
+      betterUs.append(u)
     u += 1
 
-  print 'Nusers: ', u
-  print 'No. ppr better users: ', pprBetter
+  #print 'Nusers: ', u
+  #print 'No. ppr better users: ', pprBetter
 
   pprF.close()
   gprF.close()
+  return betterUs
+
+
+def findAvgItemPerU(csrName, betterUs):
+  u = 0
+  betterUSet = set(betterUs)
+  with open(csrName, 'r') as f:
+    for line in f:
+      cols = line.strip().split()
+      nItems = len(cols)/2
+      if u not in betterUSet:
+        print u, nItems
+      u += 1
 
 
 def main():
   pprFName = sys.argv[1]
   gprFName = sys.argv[2]
-  findBetterPPR(pprFName, gprFName)
-
+  csrName = sys.argv[3]
+  betterUs = findBetterPPR(pprFName, gprFName)
+  findAvgItemPerU(csrName, betterUs)
 
 if __name__ == '__main__':
   main()
