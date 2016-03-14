862942440ac95fca2c2484c3500e69cd5874472e
mohit sharma <sharm163@umn.edu>
2016-03-14 15:20:44 -0500

    added item display

--- topBucketComp.cpp
+++ topBucketComp.cpp
@@ -170,7 +170,7 @@ void writeTopBuckRMSEs(Model& origModel, Model& fullModel, gk_csr_t* graphMat,
         //found in filtered items
         continue;
       }
-      opFile << item << " " << itemURMSE[item] << " " << itemARMSE[item];
+      opFile << item << " " << itemURMSE[item] << " " << itemARMSE[item] << std::endl;
     }
     opFile.close();
   }
