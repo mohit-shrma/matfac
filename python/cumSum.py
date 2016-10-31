ba2564fa3013d1da6b95d4f911014e6948bae833
mohit msi <sharm163@umn.edu>
2016-10-31 17:22:11 -0500

    updated cum sum

--- python/cumSum.py
+++ python/cumSum.py
@@ -8,7 +8,7 @@ ipSum[0] = ipList[0]
 for i in range(1, len(ipList)):
   ipSum[i] = ipSum[i-1] + ipList[i]
 
-ipStrSums = map(str, ipSum)
+ipStrSums = map(str, [ipSum[0], ipSum[1], ipSum[2], ipSum[4], ipSum[9]])
 print ' & '.join(ipStrSums)
 
 
