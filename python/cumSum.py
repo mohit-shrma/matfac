3cbf5946805d3a96c4fdda4104939201f09df781
mohit msi <sharm163@umn.edu>
2016-10-31 17:17:28 -0500

    updated cumSum

--- python/cumSum.py
+++ python/cumSum.py
@@ -1,7 +1,15 @@
+import sys
+
+#stdin of floats
+ipList = map(lambda x: float(x), sys.stdin.readlines())
+
+ipSum = [0 for x in ipList]
 ipSum[0] = ipList[0]
 for i in range(1, len(ipList)):
   ipSum[i] = ipSum[i-1] + ipList[i]
 
 ipStrSums = map(str, ipSum)
 print ' & '.join(ipStrSums)
+
+
 
