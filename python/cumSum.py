66c1e1cec11769adb7f64dbc748b9f49e75d819b
mohit msi <sharm163@umn.edu>
2016-10-31 17:50:54 -0500

    print pc to 3 places

--- python/cumSum.py
+++ python/cumSum.py
@@ -8,7 +8,9 @@ ipSum[0] = ipList[0]
 for i in range(1, len(ipList)):
   ipSum[i] = ipSum[i-1] + ipList[i]
 
-ipStrSums = map(str, [ipSum[0], ipSum[1], ipSum[2], ipSum[4], ipSum[9]])
+ipStrSums = map(str, ['{0:.3g}'.format(100*ipSum[0]), '{0:.3g}'.format(100*ipSum[1]),
+'{0:.3g}'.format(100*ipSum[2]), '{0:.3g}'.format(100*ipSum[4]),
+'{0:.3g}'.format(100*ipSum[9])] ) 
 print ' & '.join(ipStrSums)
 
 
