8eead97ac9626ba8b15461ed192983dcc94c96e0
mohit sharma <sharm163@umn.edu>
2016-11-07 17:07:08 -0600

    updated code

--- modelMF.h
+++ modelMF.h
@@ -32,28 +32,6 @@ class ModelMF : public Model {
     void hogTrain(const Data& data, Model& bestModel,
         std::unordered_set<int>& invalidUsers,
         std::unordered_set<int>& invalidItems) ;
-    void hogAdapTrain(const Data& data, Model& bestModel,
-        std::unordered_set<int>& invalidUsers,
-        std::unordered_set<int>& invalidItems) ;
-    void uniTrain(const Data& data, Model& bestModel,
-        std::unordered_set<int>& invalidUsers,
-        std::unordered_set<int>& invalidItems) ;
-    virtual void partialTrain(const Data& data, Model& bestModel, 
-        std::unordered_set<int>& invalidUsers,
-        std::unordered_set<int>& invalidItems) ;
-    virtual void subTrain(const Data& data, Model& bestModel,
-                        int uStart, int uEnd, int iStart, int iEnd);
-    virtual void fixTrain(const Data& data, Model& bestModel,
-                        int uStart, int uEnd, int iStart, int iEnd);
-    virtual void subExTrain(const Data& data, Model& bestModel,
-                        int uStart, int uEnd, int iStart, int iEnd);
-    virtual void computeUGrad(int user, int item, float r_ui, 
-        std::vector<double> &uGrad);
-    virtual void computeIGrad(int user, int item, float r_ui, 
-        std::vector<double> &iGrad);
-    void updateAdaptiveFac(std::vector<double> &fac, std::vector<double> &grad,
-        std::vector<double> &gradAcc);
-    void gradCheck(int u, int item, float r_ui);
 };
 
 
